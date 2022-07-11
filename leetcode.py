63f35708379fb8e5a57601b7f0f99a3aa2c7bb91
Hongru Liu <mortimerliu0@gmail.com>
2022-07-11 10:31:28 -0400

    07/11/2022 (759)

--- leetcode.py
+++ leetcode.py
@@ -445,6 +445,68 @@ def insert(self, head: 'Optional[Node]', insertVal: int) -> 'Node':
         cur = cur.next
 
 
+"""759. Employee Free Time
+
+We are given a list `schedule` of employees, which represents the 
+working time for each employee.
+
+Each employee has a list of non-overlapping `Intervals`, and these 
+intervals are in sorted order.
+
+Return the list of finite intervals representing **common, 
+positive-length free time** for *all* employees, also in sorted order.
+
+(Even though we are representing `Intervals` in the form `[x, y]`, 
+the objects inside are `Intervals`, not lists or arrays. For example, 
+`schedule[0][0].start = 1`, `schedule[0][0].end = 2`, and 
+`schedule[0][0][0]` is not defined). Also, we wouldn't include intervals 
+like [5, 5] in our answer, as they have zero length.
+
+
+**Example 1:**
+
+```
+Input: schedule = [[[1,2],[5,6]],[[1,3]],[[4,10]]]
+Output: [[3,4]]
+Explanation: There are a total of three employees, and all common
+free time intervals would be [-inf, 1], [3, 4], [10, inf].
+We discard any intervals that contain inf as they aren't finite.
+```
+
+**Example 2:**
+
+```
+Input: schedule = [[[1,3],[6,7]],[[2,4]],[[2,5],[9,12]]]
+Output: [[5,6],[7,9]]
+```
+"""
+
+class Interval:
+    def __init__(self, start: int = 0, end: int = 0):
+        self.start = start
+        self.end = end
+
+
+def employeeFreeTime(self, schedule: List[List[Interval]]) -> List[Interval]:
+    queue = []
+    for j, s in enumerate(schedule):
+        heapq.heappush(queue, [s[0].start, 0, j])
+    
+    free_time = []
+    cur_end = -1
+    while queue:
+        _, i, j = heapq.heappop(queue)
+        s = schedule[j]
+        if cur_end >= 0 and s[i].start > cur_end:
+            free_time.append(Interval(cur_end, s[i].start))
+        cur_end = max(cur_end, s[i].end)
+        i += 1
+        if i < len(s):
+            heapq.heappush(queue, [s[i].start, i, j])
+    
+    return free_time
+
+
 """791. Custom Sort String
 
 You are given two strings order and s. All the words of `order` are 
