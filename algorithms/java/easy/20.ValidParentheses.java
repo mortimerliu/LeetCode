26c5bc5e217872352915da1561715597e1a3531f
Hongru Liu <hl3148@columbia.edu>
2023-06-01 18:53:41 -0400

    20.ValidParentheses

---
+++ algorithms/java/easy/20.ValidParentheses.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (40.63%)
+ * Likes:    20023
+ * Dislikes: 1209
+ * Total Accepted:    3.4M
+ * Total Submissions: 8.3M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: s = "()"
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: s = "()[]{}"
+ * Output: true
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: s = "(]"
+ * Output: false
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length <= 10^4
+ * s consists of parentheses only '()[]{}'.
+ *
+ *
+ */
+
+// @lc code=start
+
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        // stack
+        Deque<Character> stack = new ArrayDeque<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) {
+                stack.push(c);
+            } else if (stack.isEmpty() || !stack.pop().equals(map.get(c))) {
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
+// @lc code=end
